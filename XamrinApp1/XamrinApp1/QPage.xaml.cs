d1fe07400af0c9a87a2aade97ef0b2fa7617b247
sharmila8989 <sharmila8989@users.noreply.github.com>
2018-04-20 08:56:56 +1000

    initial commit

---
+++ XamrinApp1/XamrinApp1/QPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamrinApp1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class QPage : ContentPage
+	{
+        int pos = 0;
+        List<string> Qoutes = new List<string>()
+        {
+            "Make hay while sunshines",
+            "Qoute 2","Qoute 3"
+        };
+        public QPage ()
+		{
+			InitializeComponent ();
+            LableQoutes.Text = Qoutes[pos];
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            pos++;
+            if (pos >= Qoutes.Count) pos = 0;
+
+            LableQoutes.Text = Qoutes[pos];
+
+           
+
+        }
+    }
+}
